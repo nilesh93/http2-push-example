917fe23766420e514199ed0a9b88ad0aab79c2d0
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2018-12-14 08:39:29 -0800

    minor fix

--- src/server.js
+++ src/server.js
@@ -5,18 +5,29 @@ const path = require('path')
 // eslint-disable-next-line
 const http2 = require('http2')
 const helper = require('./helper')
+ 
+// const options = {
+//    cert: fs.readFileSync(path.join(__dirname, '../ssl/cert.pem')),
+//   key: fs.readFileSync(path.join(__dirname, '../ssl/key.pem'))
+// };
+// const server = http2.createSecureServer(options);
+// server.on('stream', (stream, requestHeaders) => {
+//   stream.respond();
+//   stream.end('secured hello world!');
+// });
+// server.listen(43);
 
 const { HTTP2_HEADER_PATH } = http2.constants
 const PORT = process.env.PORT || 3000
 const PUBLIC_PATH = path.join(__dirname, '../public')
 
 const publicFiles = helper.getFiles(PUBLIC_PATH)
-const server = http2.createSecureServer({}, onRequest)
+// const server = http2.createSecureServer({}, onRequest)
 
-// const server = http2.createServercreateSecureServer({
-//   cert: fs.readFileSync(path.join(__dirname, '../ssl/cert.pem')),
-//   key: fs.readFileSync(path.join(__dirname, '../ssl/key.pem'))
-// }, onRequest)
+const server = http2.createSecureServer({
+  cert: fs.readFileSync(path.join(__dirname, '../ssl/cert.pem')),
+  key: fs.readFileSync(path.join(__dirname, '../ssl/key.pem'))
+}, onRequest)
 // Push file
 function push(stream, path) {
   const file = publicFiles.get(path)
